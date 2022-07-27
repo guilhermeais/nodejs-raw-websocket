154e6777f71bb9eb2e890b3a43559cf066f85197
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-07-27 08:18:35 -0300

    feat: handling the dinary data received from client

--- server.mjs
+++ server.mjs
@@ -2,6 +2,13 @@ import { createServer } from 'node:http'
 import crypto from 'crypto'
 const PORT = 1337
 const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+const SEVEN_BITS_INTEGER_MARKER = 125
+const SIXTEEN_BITS_INTEGER_MARKER = 126
+const SIXTYFOUR_BITS_INTEGER_MARKER = 127
+
+const MASK_KEY_BYTES_LENGTH = 4 
+
+const FIRST_BIT = 128 // One bit
 
 function handle(request, response) {
   response.writeHead(200)
@@ -11,13 +18,78 @@ function handle(request, response) {
 const server = createServer(handle)
 .listen(PORT, () => console.log(`server listening on port ${PORT}`))
 
-server.on('upgrade', onSocketUpgrade)
+server.on('upgrade', onSocketUpgrade )
 
 function onSocketUpgrade(request, socket, head) {
   const {'sec-websocket-key': webClientSocketKey} = request.headers
   console.log(`${webClientSocketKey} connected!`);
   const headers = prepareHandShakeHeaders(webClientSocketKey)
   socket.write(headers)
+  socket.on('readable', () => onSocketReadable(socket))
+}
+
+/**
+ * 
+ * @param {import('stream').Duplex} socket 
+ */
+function onSocketReadable(socket) {
+  // consume optcode (first byte)
+  // 1 -> 1 byte - 8 bits
+  socket.read(1)
+  const [markerAndPayloadLength] = socket.read(1)
+  // Because the first bit is always 1 for clint-to-server messages
+  // You can subtract one bit (128 or '10000000') from this byte to get rid of the MAS bit
+  const lengthIndicatorBits = markerAndPayloadLength - FIRST_BIT
+  let messageLength = 0
+
+  if(lengthIndicatorBits <= SEVEN_BITS_INTEGER_MARKER) {
+    // 7 bits or less -> 1 byte
+    messageLength = lengthIndicatorBits
+  } else { 
+   throw new Error(`you message is too long! for now we don't handle 64 bits message`)
+  }
+
+  const maskKey = socket.read(MASK_KEY_BYTES_LENGTH)
+  const encoded = socket.read(messageLength)
+  const decoded = unMask(encoded, maskKey)
+  const received = decoded.toString('utf-8')
+  
+  const data = JSON.parse(received)
+  console.log(data);
+}
+(1).toString()
+function unMask(encodedBuffer, maskKey) {
+  const finalBuffer = Buffer.from(encodedBuffer)
+  // because the mask key has only 4 bytes
+  // index % 4 === 0, 1, 2, 3; that are the index bits needed to decode the message
+
+  // ^ is bitwise XOR
+  // returns 1 if both are different
+  // returns 0 if both are equal
+
+  // (71).toString(2).padStart(8, "0") = 0 1 0 0 0 1 1 1 -> binary representation of 71
+  // (53).toString(2).padStart(8, "0") = 0 0 1 1 0 1 0 1 -> binary representation of 53
+  //                                     0 1 1 1 0 0 1 0 -> XOR result
+
+  // Parsing the 01110010 XOR result, to decimal using parseInt('01110010', 2), we get 114, that is a char code
+  // and if we search this char code, with String.fromCharCode(114), we get the character 'r'
+  const fillWithEigthZeros= (t) => t.padStart(8, "0")
+  const toBinary = (t) => fillWithEigthZeros(t.toString(2))
+  const fromBinaryToDemail = (t) => parseInt(toBinary(t), 2)
+  const getCharFromBinary = (t) => String.fromCharCode(fromBinaryToDemail(t))
+
+  for(let i = 0; i < finalBuffer.length; i++) {
+    finalBuffer[i] = encodedBuffer[i] ^ maskKey[i % MASK_KEY_BYTES_LENGTH]
+    const logger = {
+      unmaskingCalc: `${toBinary(encodedBuffer[i])} ^ ${toBinary(maskKey[i % MASK_KEY_BYTES_LENGTH])}(maskKey at ${i % MASK_KEY_BYTES_LENGTH}) = ${toBinary(finalBuffer[i])}`,
+      decoded: getCharFromBinary(finalBuffer[i]),
+      
+    }
+
+    console.log(logger);
+  }
+
+  return finalBuffer
 }
 
 function prepareHandShakeHeaders(id) {
